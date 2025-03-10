e01ba2d15d31342eb0ff9384c52dd218639dc110
priyadharshinitc <priyadharshinihp@gmail.com>
2025-03-10 20:26:22 +0530

    Added alt attribute to img tag

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
   return (
     <>
         <section className={`not-found section-message flex flex-col items-center gap-3 text-center mt-1/4`}>
-          <img src={notFound} className="message-img h-[180px] w-fit" />
+          <img src={notFound} className="message-img h-[180px] w-fit" alt="Not Found" />
           <h1>Page not found</h1>
           <h4 className="regular-txt">City not found. Make sure to enter a valid city name.</h4>
         </section>
--- src/components/SearchCity.jsx
+++ src/components/SearchCity.jsx
@@ -5,7 +5,7 @@ const SearchCity = () => {
   return (
     <>
         <section className={`search-city section-message flex flex-col items-center gap-3 text-center mt-1/4`}>
-          <img src={searchCity} className="message-img h-[180px] w-fit" />
+          <img src={searchCity} className="message-img h-[180px] w-fit" alt="Search City" />
           <h1>Search City</h1>
           <h4 className="regular-txt">Find out the weather conditions of the city</h4>
         </section>
